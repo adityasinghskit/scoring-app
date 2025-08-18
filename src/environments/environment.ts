e48e6be97649715c060b92e5d02ce6c604b06de1
adityasinghskit@gmail.com <adityasinghskit@gmail.com>
2025-08-18 08:32:11 +0530

    removed keys: removed supabase keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://ikmwquiyeodjiljkrrhc.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrbXdxdWl5ZW9kamlsamtycmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExOTAyODUsImV4cCI6MjA2Njc2NjI4NX0.-sETWRXp36sDl5lDx81Osfrwz_Df1ITM9E8z2xoUcys',
-} 
+  supabaseUrl: 'SUPABASE_URL',
+  supabaseKey: 'SUPABASE_KEY',
+}
