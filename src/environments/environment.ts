ef366b7190e01a6813da9028acc8a3bb31fa0fe7
adityasinghskit@gmail.com <adityasinghskit@gmail.com>
2025-08-18 08:27:18 +0530

    css fix: added responsiveness

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://ikmwquiyeodjiljkrrhc.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrbXdxdWl5ZW9kamlsamtycmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExOTAyODUsImV4cCI6MjA2Njc2NjI4NX0.-sETWRXp36sDl5lDx81Osfrwz_Df1ITM9E8z2xoUcys',
-}
+} 
